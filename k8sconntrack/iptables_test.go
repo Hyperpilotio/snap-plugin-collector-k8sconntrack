2c0d9faa0143dff9ffb03db4c2c236005711dcb3
swhsiang <swh@hsiang.io>
2017-11-08 20:25:11 +0800

    add test for ListChains function

--- k8sconntrack/iptables_test.go
+++ k8sconntrack/iptables_test.go
@@ -1,17 +1,29 @@
 package k8sconntrack
 
 import (
-    "testing"
-    . "github.com/smartystreets/goconvey/convey"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestGetIptables(t *testing.T) {
-    Convey("Configure a conntrack client", t, func() {
-        c := New("localhost:3000")
-        Convey("Should be able to derive nat table", func() {
-            table, err := c.GetIptables([]string{"filter"})
-            So(err, ShouldEqual, nil)
-            So(table, ShouldNotEqual, nil)
-        })
-    })
+	Convey("Configure a conntrack client", t, func() {
+		c := New("localhost:3000")
+		Convey("Should be able to derive nat table", func() {
+			table, err := c.GetIptables([]string{"filter"})
+			So(err, ShouldEqual, nil)
+			So(table, ShouldNotEqual, nil)
+		})
+	})
+}
+
+func TestListChains(t *testing.T) {
+	Convey("Configure a conntrack client", t, func() {
+		c := New("localhost:3000")
+		Convey("Should be able to derive nat table", func() {
+			chains, err := c.ListChains()
+			So(err, ShouldEqual, nil)
+			So(chains, ShouldNotEqual, nil)
+		})
+	})
 }
